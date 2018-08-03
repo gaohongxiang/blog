915174d0d859d724a22db710308f1d9880e752da
gaohongxiang <739698015@qq.com>
2018-08-03 18:44:55 +0800

    添加备案信息

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container"> 
         <span class="text-light"><i class="fa fa-heart text-danger"></i>持续积累，静待花开！</span>
-        <span class="text-warning pull-right"><i class="fa fa-copyright"></i>  <small></small></span>
+        <span class="text-warning pull-right"><i class="fa fa-copyright"></i>  <small> 京ICP备18040032号</small></span>
     </div>
 </footer>
